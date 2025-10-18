ce6d5dc02383bd8d2677f79500420e0e39462765
Gaurav Pratap Singh <gs492979@gmail.com>
2025-10-18 21:55:07 +0530

    login and logout controller created.

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -43,4 +43,29 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller", error);
         res.status(500).json({ message: "Internal server error" })
     }
-}
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const foundUser = await User.findOne({ email });
+        if (!foundUser) return res.status(400).json({ message: "Invalid Credentials" });
+        const verifyPassword = await bcrypt.compare(password, foundUser.password);
+        if (!verifyPassword) return res.status(400).json({ message: "Invalid Credentials" });
+        generateToken(foundUser._id, res);
+        res.status(200).json({
+            _id: foundUser._id,
+            fullName: foundUser.fullName,
+            email: foundUser.email,
+            profilePic: foundUser.profilePic,
+        })
+    } catch (error) {
+        console.error("Error in login controller", error);
+        res.status(500).json({ message: "Internal server error", error });
+    }
+};
+
+export const logout = (_, res) => {
+    res.clearCookie("token");
+    res.status(200).json({ message: "User Logged out successfully" });
+};
